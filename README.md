7c5959f30bfbb1bffb1265e750fe28d029554a79
Keegan <perennialautodidact@gmail.com>
2025-02-04 18:57:17 -0800

    added knn classification

---
+++ README.md
@@ -0,0 +1,20 @@
+# Learning Machine Learning
+From the Machine Learning A-Z: AI, Python & R on Udemy.
+
+## Run locally
+### Virtual environment
+Setup a virtual environment with Venv
+
+```python -m venv .venv && . ./venv/bin/activate```
+
+
+### Install dependencies
+
+Install dependencies from `requirements.txt`
+
+```pip install -r requirements.txt```
+
+Remember to add new dependencies to `requirements.txt`
+
+### Run scripts
+```python <SCRIPT>.py```
---
+++ src/04_classification/02_k_nearest_neighbors/k_nearest_neighbors.py
@@ -0,0 +1,88 @@
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('social_network_ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the K-NN model on the Training set
+from sklearn.neighbors import KNeighborsClassifier
+
+# In order to use classic Euclidean distance, use Minoswki metric with a p value of 2
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(X_train, y_train)
+
+# Predicting a new result
+print(classifier.predict(sc.transform([[19,19000]])), end='\n\n')
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1), end='\n\n')
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm, end='\n\n')
+print(accuracy_score(y_test, y_pred), end='\n\n')
+
+
+# Visualising the Training set results
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(['#FA8072', '#1E90FF']))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(['#FA8072', '#1E90FF'])(i), label = j)
+plt.title('K-NN (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_test), y_test
+# Create a grid of points
+X1, X2 = np.meshgrid(
+    np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.25),
+    np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.25)
+)
+# Predict for each point on the grid
+Z = classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape)
+# Plot the decision boundary
+plt.contourf(X1, X2, Z, alpha=0.75, cmap = ListedColormap(['#FA8072', '#1E90FF']) )
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+# Define colors for scatter plot
+colors = ['#FA8072', '#1E90FF']
+# Plot the test set points
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(
+        X_set[y_set == j, 0], X_set[y_set == j, 1],
+        color=colors[i], label=j
+    )
+# Add titles and labels
+plt.title('K-NN (Test Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
